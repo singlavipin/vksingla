4b9843ebe1531a5a2fe3baddea001c0a5cafb8e3
Vipin Kumar singla <vipin.singla@gmail.com>
2017-01-27 19:43:19 +0530

    Adding libs

--- test-webservice/src/main/java/com/vksingla/webservice/soap/test/HelloSoap.java
+++ test-webservice/src/main/java/com/vksingla/webservice/soap/test/HelloSoap.java
@@ -2,9 +2,21 @@ package com.vksingla.webservice.soap.test;
 
 import javax.jws.WebService;
 
+import com.vksingla.stclicker.license.License;
+import com.vksingla.stclicker.license.LicenseGenerator;
+import com.vksingla.stclicker.license.LicenseType;
+
 @WebService
 public class HelloSoap {
 	public String sayHello(String name) {
 		return "Hello " + name;
+	}
+	
+	public void generateLicense() {
+		try {
+			LicenseGenerator.generateLicenseFile(new License(LicenseType.ST_CLICKER, "61192848", "*.*.*", 5, "20170101", "20180101"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
